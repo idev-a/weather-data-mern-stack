a92c2d1f3350181e31ae702d935f86d07339d23a
idev-a <ideveloper003@protonmail.com>
2018-11-02 05:33:42 +0700

    fix filter

--- server/controllers/filter.js
+++ server/controllers/filter.js
@@ -104,7 +104,7 @@ exports.getTableData = (req, res) => {
         lng: data.lng,
         team: data.team,
         stadium: data.name,
-        count: result.count || 0,
+        count: result[0].count || 0,
         wowxCount: dataWowx.count || 0,
         total_score: getData('total_score', 'Total Score', 2),
         rushing: {
